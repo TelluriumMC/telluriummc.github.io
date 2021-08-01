52545f8c60b76f981c2273694cbf3b09e76764e0
GoodPro712 <goodpro712@gmail.com>
2021-08-01 21:20:16 +1000

    Fix dev hover

--- js/developers-button.js
+++ js/developers-button.js
@@ -1,6 +1,9 @@
 $(".developers_button").click(function(){
     window.location.href = 'developers';
 });
-$(".developers_button").hover(function(){
+$(".developers_button").mouseover(function(){
     $(this).css('color', '#d3d3d3');
+});
+$(".developers_button").mouseout(function(){
+    $(this).css('color', '#fff');
 });
